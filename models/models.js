1183bbba623ec4068b9f7efea431f87e3d7bf96b
joshwilson00 <joshuawilson1605@gmail.com>
2020-10-29 10:46:47 +0000

    Models - Task can have many users, users can only be assigned to one task

---
+++ models/models.js
@@ -0,0 +1,35 @@
+const {Model, DataTypes, Sequelize} = require('sequelize');
+const path = require('path');
+const sequelize = process.env.NODE_ENV === 'test'
+    ? new Sequelize('sqlite::memory:', null, null, {dialect: 'sqlite', logging:false})
+    : new Sequelize({dialect: 'sqlite', storage: path.join(__dirname, 'data.db')});
+
+class Project extends Model {};
+class Task extends Model {};
+class User extends Model {};
+
+Project.init(
+    {
+        name: DataTypes.STRING
+    },
+    {sequelize}
+);
+Task.init(
+    {
+        name: DataTypes.STRING,
+        status: DataTypes.INTEGER
+    },
+    {sequelize}
+);
+User.init(
+    {
+        name: DataTypes.STRING
+    },
+    {sequelize}
+)
+Project.hasMany(Task, {as: 'tasks'});
+Task.belongsTo(Project);
+// User.belongsToMany(Task, {through:  Project});
+Task.hasMany(User, {as: 'users'});
+
+module.exports = {Project, Task, User, sequelize};
---
+++ models/models.test.js
@@ -0,0 +1,47 @@
+const {Project, Task, User, sequelize} = require('./models');
+
+describe('Project', () => {
+    beforeAll(async()=>{
+        await sequelize.sync();
+    });
+    test('should be able to create a new Project', async () => {
+        await Project.create({
+            name: 'Test Project'
+        });
+        const projects = await Project.findAll();
+        expect(projects.length).toBe(1);
+        expect(projects[0].name).toBe('Test Project');
+        expect(projects[0] instanceof Project).toBeTruthy();
+    })
+    test('should be able to create a new Task.', async () => {
+        const task = await Task.create({
+            name: "Go shopping",
+            ProjectId: 1
+        });
+        expect(task instanceof Task).toBeTruthy();
+        expect(task.name).toBe('Go shopping');
+        //Task should be assigned to a Project
+        expect(task.ProjectId).toBe(1);
+        const user = await User.create({
+            name: 'Jim',
+            TaskId: 1
+        });
+        const projects = await Project.findOne({where: {id: 1}, include: [{all: true, nested: true}]});
+        console.log(user);
+        console.log(projects.tasks)
+    })
+    test('should be able to create a user.', async () => {
+        const user = await User.create({
+            name: "Josh",
+        });
+        expect(user instanceof User).toBeTruthy();
+        expect(user.name).toBe('Josh');
+        await Task.create({
+            name: "Test Task",
+            ProjectId: 1,
+            UserId: 1
+        });
+        const task = await Task.findOne({where: {id: 1}, include: ["users"]});
+    })
+    
+})
