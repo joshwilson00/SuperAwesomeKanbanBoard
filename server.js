69e1b845f0b76dd5ed94aa8bb8a2c3318e7b4b52
joshwilson00 <joshuawilson1605@gmail.com>
2020-10-29 15:07:39 +0000

    Default value for status is 0 and tests to confirm

--- server.js
+++ server.js
@@ -84,7 +84,9 @@ app.post('/user/:userid/destroy', async (req, res) => {
 
 //create project, redirect back
 app.post('/project/create', async (req, res) => {
+    console.log(req.body);
     await Project.create(req.body)
+    console.log('Prokect created')
     res.redirect('back')
 })
 
