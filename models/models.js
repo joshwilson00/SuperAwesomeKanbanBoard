5c1224f6ed91dbea3d535180b0223154fc32fb88
joshwilson00 <joshuawilson1605@gmail.com>
2020-11-03 15:09:56 +0000

    Config changes for deployment

--- models/models.js
+++ models/models.js
@@ -1,8 +1,13 @@
 const {Model, DataTypes, Sequelize} = require('sequelize');
 const path = require('path');
-const sequelize = process.env.NODE_ENV === 'test'
-    ? new Sequelize('sqlite::memory:', null, null, {dialect: 'sqlite', logging:false})
-    : new Sequelize({dialect: 'sqlite', storage: path.join(__dirname, 'data.db')});
+const connectionSettings = {
+    test: {dialect: 'sqlite', storage: 'sqlite::memory:'},
+    dev: {dialect: 'sqlite', storage: path.join(__dirname, 'data.db')},
+    production: {dialect: 'postgres', protocal: 'postgres'}
+}
+const sequelize = process.env.NODE_ENV === 'production'
+    ? new Sequelize(process.env.DATABASE_URL, connectionSettings[process.env.NODE_ENV])
+    : new Sequelize(connectionSettings[process.env.NODE_ENV])
 
 class Project extends Model {};
 class Task extends Model {};
