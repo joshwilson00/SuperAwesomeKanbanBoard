0e7e780673e0114e8b4a0ae81fd548336754f598
joshwilson00 <joshuawilson1605@gmail.com>
2020-11-05 10:24:58 +0000

    Changed style on tasks box

---
+++ public/home.js
@@ -0,0 +1,45 @@
+const view = state =>`
+    <div class="topBar">
+        <h2>Super Awesome Kanban Board</h2>
+    </div>
+    <div class="userFormBar">
+        <button onclick="window.location.href='/users'" class="manageUsers button">Users</button>
+        <form class="userForm"action="/user/create" method="POST">
+            <input type="text" id="name" name="name" placeholder="Name" required> <br>
+            <input type="url" id="avatar" name="avatar" placeholder="Avatar Url" required> <br>
+            <input class="button" type="submit" value="Add User">
+        </form>
+    </div>
+    <section class="gallery">
+        ${state.projects.map(project=>{
+            return `
+            <a href="./project/${project.id}">
+            <div class="galleryItem" style="background-image: url(${project.image});">
+                <h3>${project.name}</h3>
+            </div>
+
+            </a>
+            `
+        })}
+        <div class="galleryProject">
+            <div class="addProject">
+                <h3>Add a project:</h3>
+                <form class="addProject" action="/project/create" method="POST">
+                    <input type="text" id="name" name="name" placeholder="Project Name" required> <br>
+                    <input type="url" id="image" name="image" placeholder="Image Url" required> <br>
+                    <input class="button" type="submit" value="Add Project">
+                </form>
+            </div>
+        </div>
+    </section>
+`
+
+
+const update = {
+    updateProjects: (state, projects)=>{
+        state.projects = projects;
+        return state;
+    }
+}
+
+app.run("home", state, view, update);
